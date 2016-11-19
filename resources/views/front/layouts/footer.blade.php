917db99142ed7ebd0da5441980bc0f5d2bb685ca
mhrezaei <mr.mhrezaei@gmail.com>
2016-11-19 13:13:51 +0330

    hadi

--- resources/views/front/layouts/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer id="main-footer">
-	<div class="container">
-		<a href="{{url('/')}}" id="foot-logo"><img src="{{url('assets/images/logo-bw.png')}}" width="150"></a>
-		<div class="simple-links"> <a href="#">تماس با ما</a> <a href="#">درباره ما</a> <a href="#">حریم شخصی</a> <a href="#">پرسش‌های متداول</a> </div>
-	</div>
-</footer>
